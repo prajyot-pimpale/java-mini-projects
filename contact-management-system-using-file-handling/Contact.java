8ff41a3901be642a2a0e65164996a4f9010bbc36
Prajyot Pimpale <pimpaleprajyot@gmail.com>
2026-07-23 15:39:33 +0530

    Add Contact class to manage contact details including id, name, phone, and email

---
+++ contact-management-system-using-file-handling/Contact.java
@@ -0,0 +1,41 @@
+class Contact {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Contact(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return String.format("%-4d %-15s %-15s %-25s", id, name, phone, email);
+    }
+}
